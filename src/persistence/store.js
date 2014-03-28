46f8f1a46b641cb97337ff5afa5e6cd5d6fc9eb0
kodmunki <kodmunki@gmail.com>
2014-03-28 14:20:08 -0500

    Add tempStorage for those browsers that don't support localStorage
    Client will do nothing different. All calls are identical. The system
    merely includes a fall back collection that will persist for the
    duration of the page life when there is not a localStorage

--- src/persistence/store.js
+++ src/persistence/store.js
@@ -18,9 +18,32 @@ store.prototype = {
         localStorage.clear();
         return this;
     }
-}
+};
+
+function tempStore() { }
+tempStore.prototype = {
+    read: function(collectionName) {
+        var collection = __ku4tempStore.find(collectionName);
+        return ($.exists(collection))
+            ? $.ku4collection.deserialize(collection).store(this)
+            : $.ku4collection(collectionName).store(this);
+    },
+    write: function(collection) {
+        __ku4tempStore.update(collection.name(), collection.serialize());
+        return this;
+    },
+    remove: function(collection) {
+        __ku4tempStore.remove(collection.name());
+        return this;
+    },
+    __delete: function() {
+        __ku4tempStore.clear();
+        return this;
+    }
+};
+var __ku4tempStore = $.dto();
 
 $.ku4store = function() {
-    if(!$.exists(localStorage)) throw new Error("This browser does not support $.store");
+    if(!$.exists(localStorage)) return new tempStore();
     else return new store();
-}
+};
